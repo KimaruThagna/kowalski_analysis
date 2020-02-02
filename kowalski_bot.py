a4b534321e9e75c57efd5bb1e8c74bfcb4e0976e
Kimaru thagana <macbuntu@localhost.localdomain>
2020-02-02 21:02:13 +0300

    refactor return message of sentiment analyzer

--- kowalski_bot.py
+++ kowalski_bot.py
@@ -1,15 +1,24 @@
-import tweepy, os
+import tweepy
+import logging
+from config.config import create_api
+import time
 
-# Authenticate to Twitter
-auth = tweepy.OAuthHandler(os.environ.get("CONSUMER_KEY"), os.environ.get("CONSUMER_SECRET"))
-auth.set_access_token(os.environ.get("ACCESS_TOKEN"), os.environ.get("ACCESS_TOKEN_SECRET"))
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
 
-# Create API object
-api = tweepy.API(auth)
-try:
-    api.verify_credentials()
-    print("Authentication OK")
-except:
-    print("Error during authentication")
-# Create a tweet
-api.update_status("Hello Tweepy")
+def listen(api, keywords, since_id):
+    logger.info("Retrieving mentions")
+    new_since_id = since_id
+    for tweet in tweepy.Cursor(api.mentions_timeline, since_id=since_id).items():
+        new_since_id = max(tweet.id, new_since_id)
+        if tweet.in_reply_to_status_id is not None: # check if the current tweet is a reply to another tweet.
+
+            logger.info(f"Answering to {tweet.user.name}")
+            if not tweet.user.following: # follow the user who summoned the bot
+                tweet.user.follow()
+
+            api.update_status(
+                status="Please reach us via DM", # results of the analysis
+                in_reply_to_status_id=tweet.id,
+            )
+    return new_since_id
--- sentiments.py
+++ sentiments.py
@@ -5,4 +5,13 @@ def sentiment_analyzer_scores(sentence):
     score = analyser.polarity_scores(sentence) # obtain polarity index of given sentence.
     # produces the index of the -ve, +ve, neutral and compound sentiment
     # the compound score is a sum of all lexicon ratings normalized between -1(very negative) to 1 (very positive)
-    return score
+    max_val = max(score["neu"], score["neg"], score["pos"])
+    max_sentiment = ''
+    full_names = {"neg": "Negative" , "pos":"Positive", "neu":"Neutral"}
+    for index, value in score.items():
+        if value == max_val:
+            max_sentiment = index
+
+    return f'Below are the sentiment scores of the query. \n Neutral score: {score["neu"]}\n ' \
+           f'Negative score: {score["neg"]} \n Positive score: {score["pos"]}\n' \
+           f'The general sentiment is {full_names[max_sentiment]}'
