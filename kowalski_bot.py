ab268cc068242457eeb499193622d84eec0b4100
Kimaru thagana <macbuntu@localhost.localdomain>
2020-02-03 14:18:28 +0300

    cleanup retrieve tweets ans sentiment analyser function

--- kowalski_bot.py
+++ kowalski_bot.py
@@ -1,13 +1,13 @@
 import tweepy, logging, time
 from config.config import create_api
 from sentiments import sentiment_analyzer_scores
-
+from retrieve_tweets import retrieve_tweets
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
 def listen(api, keywords, since_id):
     logger.info("Retrieving mentions")
-    main_tweet = ""
+
     new_since_id = since_id
     for tweet in tweepy.Cursor(api.mentions_timeline, since_id=since_id).items():
         new_since_id = max(tweet.id, new_since_id)
@@ -17,7 +17,7 @@ def listen(api, keywords, since_id):
         if not tweet.user.following: # follow the user who summoned the bot
             tweet.user.follow()
         api.update_status(
-                status= sentiment_analyzer_scores(main_tweet), # results of the analysis
+                status= sentiment_analyzer_scores(retrieve_tweets(tweet.text.lower())), # results of the analysis
                 in_reply_to_status_id=tweet.id,
             )
     return new_since_id
--- retrieve_tweets.py
+++ retrieve_tweets.py
@@ -1,6 +1,10 @@
-import tweepy, logging, time
 from config.config import create_api
+from sentiments import sentiment_analyzer_scores
+api = create_api()
 
-def retrieve_tweets(keyword):
-    for tweet in api.search(q=keyword, lang="en", rpp=10):
-        print(f"{tweet.user.name}:{tweet.text}")
+def retrieve_tweets(keyword, number=100):
+    tweet_block = ''
+    for tweet in api.search(q=keyword, lang="en", rpp=number):  # most recent 100 public tweets that contain the query word
+        tweet_block = f'{tweet_block} {tweet.text}'  # accumulate all tweets into one major string
+
+    return tweet_block  # derive a general sentiment from all the tweets
